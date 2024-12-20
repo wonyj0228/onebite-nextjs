2f913ef3d17f884fdcaf4138a86dc805571f2b45
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-12-21 03:30:32 +0900

    스타일링 & 레이아웃

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,7 +1,17 @@
+// CSS Module
+import { ReactNode } from 'react';
+import style from './index.module.css';
+import SearchableLayout from '@/components/searchable-layout';
+
 export default function Home() {
   return (
     <>
-      <h1>인덱스</h1>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
     </>
   );
 }
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
