9d150a66856cbf520365619281a1053ac218b336
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2024-12-25 00:43:09 +0900

    ISG & SEO 설정

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,22 +1,10 @@
 import SearchableLayout from '@/components/searchable-layout';
 import { ReactNode, useEffect, useState } from 'react';
 import BookItem from '@/components/book-item';
-// import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
 import { useRouter } from 'next/router';
 import { IBook } from '@/types';
-
-// context : 현재 브라우저로 받은 모든 요청에 대한 정보가 담겨있음
-// export const getServerSideProps = async (
-//   context: GetServerSidePropsContext
-// ) => {
-//   const q = context.query.q;
-//   const books = await fetchBooks(q as string);
-
-//   return {
-//     props: { books },
-//   };
-// };
+import Head from 'next/head';
 
 export default function Page() {
   const [books, setBooks] = useState<IBook[]>([]);
@@ -33,14 +21,25 @@ export default function Page() {
     if (q) {
       fetchSearchResult();
     }
-  }, []);
+  }, [q]);
 
   return (
-    <div>
-      {books.map((book) => (
-        <BookItem key={book.id} {...book} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>한입북스 : 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스 - 검색결과" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </div>
+    </>
   );
 }
 
